ee1526ff1efe4bc6ea3a8c2d401dd732f13fa640
Julian <julian@jgroen.com>
2020-08-23 22:53:45 -0600

    synced judge card, pulling basic show stats for compare

--- src/components/JudgeCard.js
+++ src/components/JudgeCard.js
@@ -7,11 +7,79 @@ class JudgeCard extends StageComponent {
         this.secondShow = secondShow;
     }
 
+    update() {
+        super.update();
+        this.checkShowStages();
+    }
+
     markup() {
-        console.log(this.firstShow);
+        switch (this.stateData[this.stageId]) {
+            case 0:
+                return this.displayVersus();
+                break;
+
+            case 1:
+                return this.displayLoader();
+                break;
+
+            case 2:
+                return this.displayStats();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    checkShowStages() {
+        const firstStageId = this.firstShow.stageId;
+        const secondStageId = this.secondShow.stageId;
+        const stateString = [this.stateData[firstStageId], this.stateData[secondStageId]].join('');
+        if (stateString == '01' || stateString == '10') {
+            if (this.stateData[this.stageId] == '0') {
+                this.nextStage();
+            }
+        }
+        if (stateString == '22') {
+            if (this.stateData[this.stageId] == '1') {
+                this.nextStage();
+            }
+        }
+        if (stateString == '02' || stateString == '20') {
+            if (this.stateData[this.stageId] == '2') {
+                this.nextStage();
+            }
+        }
+    }
+
+    displayVersus() {
         return /*html*/ `
             <h1>VS</h1>
         `;
+    }
+
+    displayLoader() {
+        return this.displayVersus();
+    }
+
+    displayStats() {
+        const { firstItems, secondItems } = this.pullShowStats();
+        return /*html*/ `
+            <p>
+                ${firstItems.map((item) => item)};
+            </p>
+            <p>
+                ${secondItems.map((item) => item)};
+            </p>
+        `;
+    }
+
+    pullShowStats() {
+        const firstItemContainer = document.querySelector('#first-card .show-item-container');
+        const firstItems = [...firstItemContainer.querySelectorAll('.show-item')].map((item) => item.innerHTML);
+        const secondItemContainer = document.querySelector('#second-card .show-item-container');
+        const secondItems = [...secondItemContainer.querySelectorAll('.show-item')].map((item) => item.innerHTML);
+        return { firstItems, secondItems };
     }
 }
 
